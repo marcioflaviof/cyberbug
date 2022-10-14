bcd415b752d6e0275c74fea6a42bc96a4c74f467
Marcio Flavio <mflaviof1995@gmail.com>
2022-10-14 11:53:41 -0300

    Initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>CyberBug 2022</title>
+        <meta name="description" content="Copy of cyberpunk 2077 homepage" />
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      "blue-100": "#02d8f3",
+      black: "#000000",
+      "gray-200": "#cdcdcd",
+      "gray-800": "#0e0e0e",
+      "green-100": "#00ffd2",
+      "red-light": "#ff003c",
+      yellow: "#fcee0a",
+    },
+    fontFamily: {
+      "pro-book": ["BlenderProBook", "sans-serif"],
+      "pro-bold": ["BlenderProBold", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
