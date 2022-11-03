6ef6d32439eee29ca4cc565319e1e6eda1a36ce3
Marcio Flavio <mflaviof1995@gmail.com>
2022-11-03 18:39:50 -0300

    Improve font handling

--- src/components/molecules/Terms/Terms.tsx
+++ src/components/molecules/Terms/Terms.tsx
@@ -1,5 +1,4 @@
 import { useTranslation } from "react-i18next";
-import { blenderBold } from "src/app/layout";
 
 const Terms = () => {
   const { t } = useTranslation();
@@ -7,22 +6,22 @@ const Terms = () => {
   return (
     <ul className="flex flex-col gap-6 text-center text-black mb-4">
       <li>
-        <a className="font-blender-bold">
+        <a>
           {t("termsandprivacy")} {"<"}
         </a>
       </li>
       <li>
-        <a className="font-blender-bold">
+        <a>
           {t("careers")} {"<"}
         </a>
       </li>
       <li>
-        <a className="font-blender-bold">
+        <a>
           {t("useragreement")} {"<"}
         </a>
       </li>
       <li>
-        <a className={blenderBold.className}>
+        <a>
           {t("guidelines")} {"<"}
         </a>
       </li>
