949d3002552ba288387ceee908e01c50761e78f1
Marcio Flavio <mflaviof1995@gmail.com>
2022-12-05 15:37:14 -0300

    Add swiper and videos

--- src/components/molecules/Media/Media.tsx
+++ src/components/molecules/Media/Media.tsx
@@ -1,16 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/legacy/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
 import { SectionTitle } from "@atoms/SectionTitle/SectionTitle";
 import { MediaButton } from "./MediaButton";
 
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Videos } from "./Videos";
+
 const Media = () => {
   return (
-    <section className="h-full w-full bg-black pb-4 px-4">
+    <section className="flex flex-col items-center h-full w-full bg-black pb-4 px-4">
       <SectionTitle title="Media" />
-      <div className="text-gray-200 font-blender-bold flex flex-wrap justify-center max-w-xl w-full">
+      <div className="text-gray-200 font-blender-bold flex flex-wrap max-w-xl w-full">
         <MediaButton title="Videos" active />
         <MediaButton title="Screenshots" roundedBorder="topRight" />
         <MediaButton title="Wallpapers" roundedBorder="bottonLeft" />
         <MediaButton title="Concept Art" />
       </div>
+      <Videos />
     </section>
   );
 };
